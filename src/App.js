fdfc98587818d71803c837292179477f5a42adc1
mlabato <m_labato@hotmail.com>
2022-12-16 10:13:55 -0300

    font config added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div >
-        
+    <div className="font-montserrat">
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
     </div>
   );
 }
