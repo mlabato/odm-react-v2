d930b47939235a03008fa1f2a460d508a114477f
mlabato <m_labato@hotmail.com>
2022-12-19 16:51:18 -0300

    Header links added

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,8 +6,8 @@ const Header = (props) => {
   return (
     <section>
       <article className="bg-[#D9D9D9] flex flex-row items-center p-[.5rem]">
-        <FaWhatsapp className="mx-[.75rem] text-[1rem] text-[#A61212]" />
-        <FaInstagram className="mr-[1rem] text-[1rem] text-[#A61212]" />
+        <a href="https://api.whatsapp.com/send/?phone=5492323517210&text&type=phone_number&app_absent=0"><FaWhatsapp className="mx-[.75rem] text-[1rem] text-[#A61212]" /></a>
+        <a href="https://www.instagram.com/outletde_mates/"><FaInstagram className="mr-[1rem] text-[1rem] text-[#A61212]" /></a>
         <p className="text-[#A61212] text-[0.8rem] mx-auto">
           {" "}
           Aceptamos Mercado Pago y todas las tarjetas de crédito. Envíos a todo
@@ -17,7 +17,7 @@ const Header = (props) => {
       
       <article className="bg-[#F8F9FA] border-b-2 border-[#A61212]">
         <div className=" flex flex-row items-center justify-between mx-[10rem] py-[1rem]">
-          <img src="/images/logo.png" alt="" className="w-[8rem]" />
+          <a href="/"><img src="/images/logo.png" alt="" className="w-[8rem]" /></a>
           <SearchBar />
           <CartButton onClick={props.onClick} />
         </div>
