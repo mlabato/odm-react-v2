336b754228556b5eb07b3d06a9449ed4a88fdb74
mlabato <m_labato@hotmail.com>
2022-12-22 11:12:23 -0300

    link to home added

--- src/components/layout/CategoryDescriptionContainer.js
+++ src/components/layout/CategoryDescriptionContainer.js
@@ -11,7 +11,7 @@ const categories = [
     category: "Mate Imperial",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a est nec quam luctus aliquet vitae eget ex. Nulla fermentum purus non augue condimentum, eget suscipit lectus blandit. Ut massa turpis, lobortis non est id, pellentesque egestas lacus. In at leo in magna rhoncus suscipit eget eget libero. Vivamus sodales, elit ac condimentum tincidunt, augue eros sagittis leo, vitae suscipit lorem nisl eget elit. Donec id arcu a ante egestas aliquam a ut nisi. Aliquam vel massa quis tortor dictum malesuada. Curabitur cursus convallis tortor ut elementum. Fusce in ex quis felis aliquam porta. Proin quam tortor, molestie at posuere consectetur, vestibulum id justo.",
-    image:"/images/imperial.jpg",
+    image: "/images/imperial.jpg",
   },
   {
     category: "Bombillas",
@@ -33,19 +33,30 @@ const categories = [
   },
 ];
 
-const mappedCategories = categories.map(category => {
-    return(
-        <CategoryCard title={category.category} description={category.description} image={category.image} />
-    )
-})
+const mappedCategories = categories.map((category) => {
+  return (
+    <CategoryCard
+      title={category.category}
+      description={category.description}
+      image={category.image}
+    />
+  );
+});
 
 const CategoryDescriptionContainer = () => {
   return (
-    <section className="bg-[#F8F9FA] py-[3rem]">
-    <article className="text-center font-bold text-[3rem]">Nuestros productos</article>
-      <article className="mx-[10rem]">
-        {mappedCategories}
+    <section className="bg-[#F8F9FA] py-[3rem] flex flex-col justify-center">
+      <article className="text-center font-bold text-[3rem]">
+        Nuestros productos
       </article>
+      <article className="mx-[10rem]">{mappedCategories}</article>
+
+      <a
+        href="/"
+        className=" text-center font-bold mt-[4rem] text-[1.5rem] hover:text-[#A61212]"
+      >
+        Volver a la Home
+      </a>
     </section>
   );
 };
