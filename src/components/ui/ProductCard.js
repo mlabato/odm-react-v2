d9b699153a15e2f2e5fbb2406843e4aa8ed2946f
mlabato <m_labato@hotmail.com>
2022-12-22 12:29:40 -0300

    PriceCard imported to ProductCard

--- src/components/ui/ProductCard.js
+++ src/components/ui/ProductCard.js
@@ -3,6 +3,7 @@ import { FaCartPlus } from "react-icons/fa";
 import { FaEye } from "react-icons/fa";
 
 import CartContext from "../../store/cart-context";
+import PriceCard from "./PriceCard";
 
 const ProductCard = (props) => {
   const cartCtx = useContext(CartContext);
@@ -24,19 +25,23 @@ const ProductCard = (props) => {
   return (
     <article className="w-[20rem] hover:bg-white p-[1rem] m-[1rem] hover:rounded-lg border-b-2 border-[#D9D9D9] hover:border-none ">
       <img src={props.image} alt=""></img>
-      <div className="text-center pt-[1rem] h-[4rem]">{props.model}</div>
-      <div className="text-[#A61212] font-bold text-[1.5rem] my-[.5rem] text-center">
-        {"$ " + props.price}
+      <div className=" pt-[1rem] h-[4rem] text-[1.2rem]">{props.model}</div>
+      <div className="text-[#A61212] font-bold text-[1.5rem] my-[.5rem] ">
+        <PriceCard price={props.price} discount={props.discount} />
       </div>
-      <div className="flex flex-row text-[#A61212] mx-[5rem] justify-around items-center text-[1.5rem] ">
+      <div className="flex flex-row text-[#A61212] mx-[5rem] justify-around items-center mt-[2rem] ">
         <button
-          className={`${cartIsClicked && "animate-wiggle"} text-[#A61212]`}
+          className={`${cartIsClicked && "animate-wiggle"} text-[#A61212] `}
           onClick={addToCartHandler}
           onAnimationEnd={() => setCartIsClicked(false)}
         >
-          <FaCartPlus />
+          <FaCartPlus className="text-[2.2rem] hover:text-[#000000]" />
         </button>{" "}
-        <a href={"/productos/" + props.id}><FaEye /></a>
+        <a href={"/productos/" + props.id}>
+          <FaEye
+            className="text-[2.2rem] hover:text-[#000000]"
+          />
+        </a>
       </div>
     </article>
   );
