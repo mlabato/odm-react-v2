5bd327bc8c84dcabcc59dcf5e4fd551c68a2e2be
mlabato <m_labato@hotmail.com>
2023-01-05 17:01:18 -0300

    FilterBar  turned operative

--- src/components/layout/ProductsContainer.js
+++ src/components/layout/ProductsContainer.js
@@ -14,7 +14,7 @@ const ProductsContainer = () => {
 
   return (
     <section className="w-3/4  flex flex-row flex-wrap">
-      {console.log(productsCtx.isLoading)}
+      
       {isLoading && <div className="mx-auto"><Spinner /></div>}
       {!isLoading && stockedProducts?.map((product, i) => {
         return <ProductCard key={i} model={product.model} price={product.price} id={product.id} category={product.category} discount={product.discount} image={product.image}/>;
--- src/components/ui/FilterBar.js
+++ src/components/ui/FilterBar.js
@@ -1,15 +1,32 @@
-import {TbArrowsUpDown} from "react-icons/tb"
+import { useContext, useState } from "react";
+import ProductContext from "../../store/product-context";
+
+import { TbArrowsUpDown } from "react-icons/tb";
 
 const FilterBar = () => {
+  const productsCtx = useContext(ProductContext);
+  const [filter, setFilter] = useState(true);
+
+  const filterClickHandler = (e) => {
+    setFilter(!filter);
+    if (!filter) {
+      productsCtx.updateUrl("/low-to-high-price-products");
+    } else {
+      productsCtx.updateUrl("/high-to-low-price-products");
+    }
+  };
+
   return (
     <article className="flex  justify-end">
       {" "}
-      <div className="flex flex-row items-center justify-between border-2 border-[#A61212] text-[#A61212] w-[17rem] rounded-lg p-[.5rem]">
-        Más barato al más caro
-        <TbArrowsUpDown className="ml-[2rem] text-[1.5rem]"/>
-        
-      </div>
-      
+      <button
+        className="flex flex-row items-center justify-between border-2 border-[#A61212] text-[#A61212] w-[17rem] rounded-lg p-[.5rem]"
+        onClick={filterClickHandler}
+      >
+        {filter && "Más barato al más caro"}
+        {!filter && "Más caro al más barato"}
+        <TbArrowsUpDown className="ml-[2rem] text-[1.5rem]" />
+      </button>
     </article>
   );
 };
